16dc0f316d767d1a392318d47d2607875a016d49
laxpuzzle <laxajaxmail@gmail.com>
2018-01-17 23:30:32 +0800

    add map

---
+++ diancan-backend-ui/src/components/map/baidumap.vue
@@ -0,0 +1,25 @@
+<template>
+   <div></div>
+</template>
+
+<script>
+export default {
+    props: {
+      zoom : 1,
+      defaultAddr: {
+          latitude :'',
+          longitude:''
+      }
+    },
+ name:'BMap',
+ data() {
+     return {
+
+     }
+ }  
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- diancan-backend-ui/src/components/restaurantaddrmanagement/restaurantaddrmanagement.vue
+++ diancan-backend-ui/src/components/restaurantaddrmanagement/restaurantaddrmanagement.vue
@@ -1,17 +1,22 @@
 
 <template>
-   <div>
+   <div class="map">
        {{pageinfo}}
+       <div class="addr-opts"></div>
    </div>
 </template>
 
 <script>
+import BMap from '../map/baidumap.vue'
 export default {
   name :'restaurantaddrmanagement',
   data(){
       return {
           pageinfo:'this is restaurant address management page'
       }
+  },
+  components:{
+      BMap
   }  
 }
 </script>
